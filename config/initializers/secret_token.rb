3aa03e5e3c9b2670ba74bc711deed83b5e7c8d8a
Matt Darby <matt.darby@edgecase.com>
2012-09-18 16:09:15 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conclave::Application.config.secret_token = '70e0e6a57a414606516784b556522724f095cc23f0d9f3258b3768a42e0f8059f1ce88c211bbe85b4ede78ac908ebb666f3336bf4b434205eb504da037931674'

d030fb6378498affb81e7b99d7d0f30e1d9d58c5
Mike Danko <danko@theorypump.com>
2012-09-19 15:23:08 -0400

    fixed mass assignment bug
    Feel free to clean up the controller

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,25 @@
 ActiveAdmin.register User do
+
+  controller do
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(params[:user].reject {|k,v| k == 'role'}) && @user.update_attribute(:role, params[:user][:role])     
+        redirect_to admin_user_path(@user)
+      else
+        render 'edit'
+      end
+    end
+
+    def create
+      @user = User.new(params[:user].reject {|k,v| k == 'role'})
+      if @user.save
+        redirect_to admin_user_path(@user)
+      else
+        render 'new'
+      end
+    end
+  end
+
   index do
     column :role
     column :email

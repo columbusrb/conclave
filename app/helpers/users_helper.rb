b44797551fbdbafe1e92fa148014f2645a576e33
Mike Danko <danko@theorypump.com>
2012-09-21 12:23:53 -0400

    added gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def gravatar(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    image_tag "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end

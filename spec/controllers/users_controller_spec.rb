aa074365de8de3f0862fee8787cb688395adcb65
Mike Danko <danko@theorypump.com>
2012-09-21 12:24:06 -0400

    this was broken and didn't have a test.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,13 @@ describe UsersController do
     sign_in @user
   end
 
+  describe "GET 'index'" do
+    it "returns http success" do
+      get :index
+      response.should be_success
+    end
+  end
+
   describe "GET 'show'" do
     
     it "should be successful" do

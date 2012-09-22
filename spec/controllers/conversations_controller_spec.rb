8a3e8d16cec0eb9bbd612b7c86d6b6983d3a0d67
Matt Darby <matt.darby@edgecase.com>
2012-09-22 18:30:58 -0400

    Fix specs for Travis

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -4,6 +4,7 @@ describe ConversationsController do
 
   describe "GET 'new'" do
     it "returns http success" do
+      Forum.stub(:find).and_return(create(:forum))
       get 'new', :forum_id => 1
       response.should be_success
     end

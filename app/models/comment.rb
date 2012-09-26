b559a5ebf8b1b3b99836698b0d6ab276d506213c
Mike Danko <danko@theorypump.com>
2012-09-26 09:23:43 -0400

    counter_cache on conversations, possible because of 7bd7100

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :conversation,
-    touch: true
+    touch: true,
+    counter_cache: true
 
   attr_accessible :conversation_id, :user_id, :content
   validates_presence_of :user, :conversation

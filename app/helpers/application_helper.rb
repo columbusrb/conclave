58b40687e30eaceb39297e25ccc585a420f2550c
Mike Danko <danko@theorypump.com>
2012-09-26 14:45:46 -0400

    icons added to UI elements. Menu changed to reflect #44.See pull request for devise changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
     options[:class] ||= "timeago"
     content_tag(:time, time.to_s, options.merge(:datetime => time.getutc.iso8601)) if time
   end
+
+  def glyphicon(icon_name, options = {})
+    output_tag = ''
+    if options[:color] == 'white'
+      output_tag = content_tag(:b, '', class: "#{icon_name} icon-white")
+    else
+      output_tag = content_tag(:b, '', class: "#{icon_name}")
+    end
+    if options[:text].present?
+      output_tag = options[:text] + ' ' + output_tag
+    end
+    return sanitize output_tag
+  end
 end

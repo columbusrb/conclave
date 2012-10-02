1eda665faa8eb7179622fb594973f73619156ffc
Mike Danko <danko@theorypump.com>
2012-10-02 08:36:28 -0400

    guest users had contributor roles.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
       can :manage, :all
     elsif user.has_role?('moderator')
       can :manage, :all
-    elsif user.has_role?('contributor')
+    elsif user.has_role?('contributor') && user.email.present?
       can :read, :all
       can :manage, Comment, user_id: user.id
       can :create, [Comment, Conversation]

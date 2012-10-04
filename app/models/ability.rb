827d8f0fa0235afcc38b6c813f1f7e00c158a90c
Matt Darby <matt.darby@edgecase.com>
2012-10-04 12:11:32 -0400

    More succinct Abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.has_role?('admin')
-      can :manage, :all
-    elsif user.has_role?('moderator')
-      can :manage, :all
-    elsif user.has_role?('contributor')
-      can :read, :all
-      can :manage, Comment, user_id: user.id
-      can :create, [Comment, Conversation]
-    else
-      can :read, :all
+    can :read, [Forum, Conversation, Comment]
+
+    if user.present?
+      if user.elevated?
+        can :manage, :all
+      else
+        can :manage, Comment, user_id: user.id
+        can :create, [Conversation, Comment]
+      end
     end
   end
 end

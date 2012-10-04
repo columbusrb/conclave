1490cd3a71877fbf0e8e8520ebf5000b3ffc5a69
David West <david.b.west@gmail.com>
2012-10-04 14:45:57 -0400

    squash this commit later

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121002184358) do
+ActiveRecord::Schema.define(:version => 20121003190519) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false

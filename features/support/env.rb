979bdb9006bb68338862cdaa796ea66d60aae1d1
Pairing <pairing@edgecase.com>
2012-10-19 16:33:16 -0400

    get oauth test passing

--- features/support/env.rb
+++ features/support/env.rb
@@ -58,19 +58,6 @@ end
 Cucumber::Rails::Database.javascript_strategy = :truncation
 Capybara.javascript_driver = :webkit
 
-Before '@oauth' do
-  AdminSetting.delete_all
-
-  AdminSetting.create do |a|
-    a.setting = "twitter_oauth_key"
-    a.value   = "LCOSViyT13oZQT07bJLqSQ"
-  end
-
-  AdminSetting.create do |a|
-    a.setting = "twitter_oauth_secret"
-    a.value   = "olERBpKi5pyxxuI5eqiwedoEAln5YBS6oN5CK5O9rc"
-  end
-end
 
 World(FactoryGirl::Syntax::Methods)
 World(Rails.application.routes.url_helpers)
